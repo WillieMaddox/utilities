6c3661530947c9c0551ff7652d69867560174c48
WillieMaddox <willie.maddox@gmail.com>
2017-04-07 14:00:51 -0500

    refactoring

--- python/spaceNetUtilities/evalTools.py
+++ python/spaceNetUtilities/evalTools.py
@@ -17,38 +17,33 @@ def iou(test_poly, truth_polys, truth_index=[]):
             intersection_result = test_poly.Intersection(truth_polys[fid])
             fidlistArray.append(fid)
 
-            if intersection_result.GetGeometryName() == 'POLYGON' or \
-                            intersection_result.GetGeometryName() == 'MULTIPOLYGON':
+            if intersection_result.GetGeometryName().endswith('POLYGON'):
                 intersection_area = intersection_result.GetArea()
                 union_area = test_poly.Union(truth_polys[fid]).GetArea()
                 iou_list.append(intersection_area / union_area)
-
             else:
                 iou_list.append(0)
 
     else:
 
         for idx, truth_poly in enumerate(truth_polys):
+
             intersection_result = test_poly.Intersection(truth_poly)
             intersection_result.GetGeometryName()
 
-            if intersection_result.GetGeometryName() == 'POLYGON' or \
-                            intersection_result.GetGeometryName() == 'MULTIPOLYGON':
+            if intersection_result.GetGeometryName() == 'POLYGON' or intersection_result.GetGeometryName() == 'MULTIPOLYGON':
                 intersection_area = intersection_result.GetArea()
                 union_area = test_poly.Union(truth_poly).GetArea()
                 iou_list.append(intersection_area / union_area)
-
             else:
-
                 iou_list.append(0)
 
     return iou_list, fidlistArray
 
 
 def score(test_polys, truth_polys, threshold=0.5, truth_index=[],
-          resultGeoJsonName = [],
-          imageId = []):
-
+          resultGeoJsonName=[],
+          imageId=[]):
     # Define internal functions
 
     # Find detections using threshold/argmax/IoU for test polygons
@@ -71,7 +66,6 @@ def score(test_polys, truth_polys, threshold=0.5, truth_index=[],
         layer.CreateField(ogr.FieldDefn("BuildingId", ogr.OFTInteger))
         layer.CreateField(ogr.FieldDefn("IOUScore", ogr.OFTReal))
 
-
     for test_poly in test_polys:
         if truth_polys:
             iou_list, fidlist = iou(test_poly, truth_polys, truth_index)
@@ -82,8 +76,9 @@ def score(test_polys, truth_polys, threshold=0.5, truth_index=[],
 
             if maxiou >= threshold:
                 true_pos_count += 1
-                truth_index.delete(fidlist[np.argmax(iou_list)], truth_polys[fidlist[np.argmax(iou_list)]].GetEnvelope())
-                #del truth_polys[fidlist[np.argmax(iou_list)]]
+                truth_index.delete(fidlist[np.argmax(iou_list)],
+                                   truth_polys[fidlist[np.argmax(iou_list)]].GetEnvelope())
+                # del truth_polys[fidlist[np.argmax(iou_list)]]
                 if resultGeoJsonName:
                     feature = ogr.Feature(layer.GetLayerDefn())
                     feature.SetField('ImageId', imageId)
@@ -103,10 +98,6 @@ def score(test_polys, truth_polys, threshold=0.5, truth_index=[],
                     feature.SetGeometry(test_poly)
 
 
-
-
-
-
         else:
             false_pos_count += 1
 
@@ -124,18 +115,15 @@ def score(test_polys, truth_polys, threshold=0.5, truth_index=[],
     if resultGeoJsonName:
         datasource.Destroy()
 
-
     false_neg_count = truth_poly_count - true_pos_count
 
     return true_pos_count, false_pos_count, false_neg_count
 
 
 def evalfunction((image_id, test_polys, truth_polys, truth_index),
-                 resultGeoJsonName = [],
-                 threshold = 0.5):
-
-
-    if len(truth_polys)==0:
+                 resultGeoJsonName=[],
+                 threshold=0.5):
+    if len(truth_polys) == 0:
         true_pos_count = 0
         false_pos_count = len(test_polys)
         false_neg_count = 0
@@ -147,7 +135,6 @@ def evalfunction((image_id, test_polys, truth_polys, truth_index),
                                                                  threshold=threshold
                                                                  )
 
-
     if (true_pos_count > 0):
 
         precision = float(true_pos_count) / (float(true_pos_count) + float(false_pos_count))
@@ -155,13 +142,11 @@ def evalfunction((image_id, test_polys, truth_polys, truth_index),
         F1score = 2.0 * precision * recall / (precision + recall)
     else:
         F1score = 0
-    return ((F1score, true_pos_count, false_pos_count, false_neg_count), image_id)
+    return (F1score, true_pos_count, false_pos_count, false_neg_count), image_id
 
 
 def create_eval_function_input((image_ids, (prop_polysIdList, prop_polysPoly), (sol_polysIdsList, sol_polysPoly))):
-
     evalFunctionInput = []
-
 
     for image_id in image_ids:
         test_polys = prop_polysPoly[np.argwhere(prop_polysIdList == image_id).flatten()]
@@ -170,5 +155,3 @@ def create_eval_function_input((image_ids, (prop_polysIdList, prop_polysPoly), (
         evalFunctionInput.append([image_id, test_polys, truth_polys, truth_index])
 
     return evalFunctionInput
-
-
