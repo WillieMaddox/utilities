64097c1f7ff1ca8997ddb7aaa452152a9e5d62e6
Willie Maddox <willie.maddox@gmail.com>
2017-04-22 10:08:15 -0500

    added IsEmpty Check off of Area

--- python/spaceNetUtilities/labelTools.py
+++ python/spaceNetUtilities/labelTools.py
@@ -613,7 +613,7 @@ def geoJsonToPASCALVOC2012(xmlFileName, geoJson, rasterImageName, im_id='',
             print(geomBufferIn.IsEmpty())
             print(geomBufferIn.IsSimple())
 
-            if not geomBufferIn.IsEmpty():
+            if geomBufferIn.GetArea() > 0.0:
                 outBufFeature = ogr.Feature(featureDefn)
                 outBufFeature.SetGeometry(geomBufferOut)
 
